1eb456e084adefbcead049942d58c8c8b40e5659
jmscherer <jscherer@uab.edu>
2024-11-07 11:56:48 -0600

    Added back in the ollama model for langchain and now working with a configurable model for the llm query endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
+from langchain.chat_models import init_chat_model
 
+configurable_model = init_chat_model(
+    configurable_fields=(
+        "model", "model_provider", "base_url", "num_thread"
+    )
+)
 
 message = [
     ("human", "Hello"),
@@ -16,4 +22,18 @@ async def root():
 @app.get("/query")
 async def query():
 
-    return { "message": "Message!" }
+    return configurable_model.invoke(
+        input = "Hello!",
+        config={
+            "configurable": {
+                "model": "llama3.2",
+                "model_provider": "ollama",
+                "temperature": 0,
+                "max_tokens": None,
+                "timeout": None,
+                "max_retries": 2,
+                "num_thread": 16,
+                "base_url": "http://138.26.49.205:11435"
+            },
+        }
+    )
