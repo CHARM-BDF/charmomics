4978f4ff798dd0f849877b98f88695a8b839f2ac
jmscherer <jscherer@uab.edu>
2026-08-04 12:57:55 -0500

    Did some more work on etc/annotations/ditto to create statistics for chromosomes and add logging

--- etc/annotations/ditto/src/generate_statistics.py
+++ etc/annotations/ditto/src/generate_statistics.py
@@ -14,7 +14,84 @@ logging.config.fileConfig('logging.conf')
 logger = logging.getLogger()
 
 ## Helper Functions ##
-def parse_tsv_to_dict(file_path, ditto_meta):
+def dictStats(meta_dict):
+    """  """
+
+    meta_dict['stats']['samples'] = len(meta_dict['variants'].keys())
+    
+    for hash in meta_dict['variants'].keys():
+        meta_dict['stats']['sum'] += float(meta_dict['variants'][hash]['ditto'])
+
+    meta_dict['stats']['mean'] = meta_dict['stats']['sum'] / meta_dict['stats']['samples']
+
+    for hash in meta_dict['variants'].keys():
+        score = float(meta_dict['variants'][hash]['ditto'])
+        meta_dict['stats']['std_dev'] += math.pow((score - meta_dict['stats']['mean']), 2)
+    
+    meta_dict['stats']['std_dev'] = math.sqrt(meta_dict['stats']['std_dev'] / meta_dict['stats']['samples'])
+
+    if meta_dict['stats']['std_dev'] == 0.0:
+        meta_dict['stats']['leftover_count'] = 1
+        meta_dict['variants'] = {}
+        return
+
+    scores_keep = []
+
+    for hash in meta_dict['variants'].keys():
+        score = float(meta_dict['variants'][hash]['ditto'])
+        z = (score - meta_dict['stats']['mean']) / meta_dict['stats']['std_dev']
+
+        if z >= -4:
+            scores_keep.append(score)
+
+    meta_dict['stats']['leftover_count'] = len(scores_keep)
+    
+    meta_dict['variants'] = {}
+
+    return
+
+def fetchDittoVariants(ditto_base_path, meta_dict):
+    """ """
+    chrom = meta_dict['chrom']
+    gene = meta_dict['gene']
+
+    ditto_path = ditto_base_path + f"/tabix/{chrom}/DITTO_{chrom}_{gene}.tsv.gz"
+
+    low = meta_dict['metadata']['low']
+    high = meta_dict['metadata']['high']
+
+    pos = low
+
+    tabix_command = f'tabix {ditto_path} {chrom}:{low}-{high}'
+    logger.info(f"  {tabix_command}")
+
+    tabix_result = subprocess.run(tabix_command.split(' '), stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
+
+    csv_reader = csv.DictReader(
+        tabix_result,
+        delimiter='\t',
+        fieldnames=["chrom", "pos", "ref", "alt", "transcript", "gene", "classification", "ditto" ]
+    )
+
+    for row in csv_reader:
+        hash = ' '.join((row['chrom'], row['pos'], row['ref'], row['alt']))
+        
+        temp_variant = meta_dict['variants'].get(hash)
+
+        if temp_variant:
+            if row['ditto'] > temp_variant['ditto']:
+                meta_dict['variants'][hash] = temp_variant
+        else:
+            meta_dict['variants'][hash] = row
+
+    logger.info(f"  {len(meta_dict['variants'])}")
+
+    return
+
+## Step 1: Gather metadata information on Ditto gene files ##
+
+# Step 1: Helper functions
+def create_gene_metadata_entry(file_path, ditto_meta):
     ditto_tsv_fieldnames = ["chrom", "pos", "ref", "alt", "transcript", "gene", "classification", "ditto" ]
     
     first_row = None
@@ -68,15 +145,27 @@ def parse_tsv_to_dict(file_path, ditto_meta):
 
     return
 
-def findGeneByPosition(meta_dict, chrom, pos):
-    """ Find with DITTO gene file to query using the metadata range """
-    
-    for gene in meta_dict[chrom].keys():
-        if meta_dict[chrom][gene]['metadata']['low'] <= int(pos) <= meta_dict[chrom][gene]['metadata']['high']:
-            return gene
+# Step 1 MetaData function
+def metadata(metadata_file, ditto_file_paths):
+    """ """
+    logger.info("[Step 1]: Open metadata file as a python dictionary")
+    with open(metadata_file, 'r') as ditto_meta_file:
+            ditto_meta_dict = json.load(ditto_meta_file)
 
-    return None
+    logger.info("[Step 1]: For each gene file in DITTO parse into a python dictionary")
+    for ditto_path in ditto_file_paths:
+        if 'UNKNOWN' in ditto_path:
+            continue
 
+        create_gene_metadata_entry(ditto_path, ditto_meta_dict)
+
+    logger.info(f"[Step 1]: Save ditto gene metadata to {metadata_file}")
+    with open(metadata_file, 'w') as ditto_meta_file:
+        json.dump(ditto_meta_dict, ditto_meta_file, indent=4)
+
+## Step 2: Match ClinVar to precomputed Ditto scores ##
+
+# Step 2: Helper Functions
 def variant_type(ref, alt):
     """ Determines the variant type: SNV; INS; DEL; INDEL """
 
@@ -88,6 +177,15 @@ def variant_type(ref, alt):
         return 'INDEL'
 
     return 'SNV'
+
+def findGeneByPosition(meta_dict, chrom, pos):
+    """ Find with DITTO gene file to query using the metadata range """
+    
+    for gene in meta_dict[chrom].keys():
+        if meta_dict[chrom][gene]['metadata']['low'] <= int(pos) <= meta_dict[chrom][gene]['metadata']['high']:
+            return gene
+
+    return None
 
 def fetchDittoScores(ditto_base_path, meta_dict, chrom, gene, pos, ref, alt, type):
     """ """
@@ -103,7 +201,7 @@ def fetchDittoScores(ditto_base_path, meta_dict, chrom, gene, pos, ref, alt, typ
             alt = '-'
 
     tabix_command = f'tabix {ditto_path} chr{chrom}:{pos}-{pos}'
-    print(tabix_command)
+    logger.info(f"  {tabix_command}")
     
     tabix_result = subprocess.run(tabix_command.split(' '), stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
 
@@ -138,136 +236,29 @@ def fetchDittoScores(ditto_base_path, meta_dict, chrom, gene, pos, ref, alt, typ
 
     return
 
-def dictStats(meta_dict):
-    """  """
-
-    meta_dict['stats']['samples'] = len(meta_dict['variants'].keys())
+# Step 2: Clinvar Match main function
+def clinvar_match(ditto_base_path, metadata_file, clinvar_base_path, chromosome):
+    """ """
     
-    for hash in meta_dict['variants'].keys():
-        meta_dict['stats']['sum'] += float(meta_dict['variants'][hash]['ditto'])
+    clinvar_plp_file = f'clinvar_{chromosome}_plp.tsv'
+    clinvar_plp_path = clinvar_base_path + clinvar_plp_file
 
-    meta_dict['stats']['mean'] = meta_dict['stats']['sum'] / meta_dict['stats']['samples']
+    logger.info(f"[Step 2]: Opening ClinVar file {clinvar_plp_path}")
+    with open(clinvar_plp_path, mode='r') as file:
+        data_lines = [line for line in file if not (line.startswith("##") or line.startswith("#"))]
 
-    for hash in meta_dict['variants'].keys():
-        score = float(meta_dict['variants'][hash]['ditto'])
-        meta_dict['stats']['std_dev'] += math.pow((score - meta_dict['stats']['mean']), 2)
-    
-    meta_dict['stats']['std_dev'] = math.sqrt(meta_dict['stats']['std_dev'] / meta_dict['stats']['samples'])
-
-    if meta_dict['stats']['std_dev'] == 0.0:
-        meta_dict['stats']['leftover_count'] = 1
-        meta_dict['variants'] = {}
-        return
-
-    scores_keep = []
-
-    for hash in meta_dict['variants'].keys():
-        score = float(meta_dict['variants'][hash]['ditto'])
-        z = (score - meta_dict['stats']['mean']) / meta_dict['stats']['std_dev']
-
-        if z >= -4:
-            scores_keep.append(score)
-
-    meta_dict['stats']['leftover_count'] = len(scores_keep)
-    
-    meta_dict['variants'] = {}
-
-    return
-
-def saveMetaDict(metadata_file, meta_dict):
-    """ """
-    with open(metadata_file, 'w') as ditto_meta_file:
-        json.dump(meta_dict, ditto_meta_file, indent=4)
-
-def fetchDittoVariants(ditto_base_path, meta_dict):
-    """ """
-    chrom = meta_dict['chrom']
-    gene = meta_dict['gene']
-
-    ditto_path = ditto_base_path + f"/tabix/{chrom}/DITTO_{chrom}_{gene}.tsv.gz"
-
-    low = meta_dict['metadata']['low']
-    high = meta_dict['metadata']['high']
-
-    pos = low
-
-    tabix_command = f'tabix {ditto_path} {chrom}:{low}-{high}'
-    print(tabix_command)
-
-    tabix_result = subprocess.run(tabix_command.split(' '), stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')
-
-    csv_reader = csv.DictReader(
-        tabix_result,
-        delimiter='\t',
-        fieldnames=["chrom", "pos", "ref", "alt", "transcript", "gene", "classification", "ditto" ]
-    )
-
-    for row in csv_reader:
-        hash = ' '.join((row['chrom'], row['pos'], row['ref'], row['alt']))
-        
-        temp_variant = meta_dict['variants'].get(hash)
-
-        if temp_variant:
-            if row['ditto'] > temp_variant['ditto']:
-                meta_dict['variants'][hash] = temp_variant
-        else:
-            meta_dict['variants'][hash] = row
-
-    print(len(meta_dict['variants']))
-
-    return
-
-def worker(package):
-    """ """
-    ditto_base_path, meta_dict = package
-
-    print(ditto_base_path)
-    print(meta_dict)
-
-    if meta_dict['clinvar']:
-        print("CLINVAR BABY!!")
-        meta_dict['variants'] = {}
-
-    if not meta_dict['clinvar']:
-        fetchDittoVariants(ditto_base_path, meta_dict)
-
-    dictStats(meta_dict)    
-
-    return meta_dict
-
-# Step 1: Gather metadata information on Ditto gene files
-def metadata(metadata_file, ditto_file_paths):
-    """ """
-    with open(metadata_file, 'r') as ditto_meta_file:
-            ditto_meta_dict = json.load(ditto_meta_file)
-
-    for ditto_path in ditto_file_paths:
-        if 'UNKNOWN' in ditto_path:
-            continue
-
-        parse_tsv_to_dict(ditto_path, ditto_meta_dict)
-
-    with open(metadata_file, 'w') as ditto_meta_file:
-        json.dump(ditto_meta_dict, ditto_meta_file, indent=4)
-
-# Step 2: Match ClinVar to Ditto and get statistics
-def clinvar(ditto_base_path, metadata_file, clinvar_base_path, chromosome):
-    """ """
-    clinvar_plp_path = clinvar_base_path + f'clinvar_{chromosome}_plp.tsv'
-
-    with open(clinvar_plp_path, mode='r') as clinvar_plp_file:
-        data_lines = [line for line in clinvar_plp_file if not (line.startswith("##") or line.startswith("#"))]
-
-    clinvar_header = ["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"]
+    logger.info(f"[Step 2]: Creating a csv_reader from the ClinVar file")
     csv_reader = csv.DictReader(
             data_lines,
             delimiter='\t',
-            fieldnames=clinvar_header
+            fieldnames=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"]
         )
     
+    logger.info(f"[Step 2]: Opening the {metadata_file}")
     with open(metadata_file, 'r') as ditto_meta_file:
         meta_dict = json.load(ditto_meta_file)
 
+    logger.info(f"[Step 2]: For each row in {clinvar_plp_file} find the precomputed Ditto score if it exists")
     for record in csv_reader:
         type = variant_type(record['REF'], record['ALT'])
         gene = findGeneByPosition(meta_dict, "chr" + record['CHROM'], record['POS'])
@@ -278,39 +269,56 @@ def clinvar(ditto_base_path, metadata_file, clinvar_base_path, chromosome):
         if not meta_dict:
             continue
 
-        print(f"{record['CHROM']}-{record['POS']}-{record['REF']}-{record['ALT']} :: {gene} :: {type}")
+        logger.info(f"  {record['CHROM']}-{record['POS']}-{record['REF']}-{record['ALT']} :: {gene} :: {type}")
 
         fetchDittoScores(ditto_base_path, meta_dict, record['CHROM'], gene, record['POS'], record['REF'], record['ALT'], type)
         
-    # Save the result
-    saveMetaDict(metadata_file, meta_dict)
-
-def savePartialMetaDict(metadata_file, meta_dict):
-    """ """
-    print(f"SAVING PARTIAL DICT TO FILE!! :: {meta_dict['gene']}")
-    with open(metadata_file, 'r') as ditto_meta_file:
-        ditto_meta_dict = json.load(ditto_meta_file)
-
-    chrom = meta_dict['chrom']
-    gene = meta_dict['gene']
-
-    ditto_meta_dict[crom][gene] = meta_dict
-
+    logger.info(f"[Step 2]: Saving the results of the score fetching to {metadata_file}")
     with open(metadata_file, 'w') as ditto_meta_file:
-        json.dump(ditto_meta_dict, ditto_meta_file, indent=4)
+        json.dump(meta_dict, ditto_meta_file, indent=4)
+    
+    return
+
+## Step 3 - Perform statistics on Ditto ##
+
+# Step 3 - Helper Functions
+
+
+def worker(package):
+    """ """
+    
+    ditto_base_path, meta_dict = package
+
+    process_name = mp.current_process().name    
+    
+    logger.info(f"[Step 3]: [{process_name}] :: Opening {ditto_base_path}")
+    if not meta_dict['clinvar']:
+        fetchDittoVariants(ditto_base_path, meta_dict)
+
+    dictStats(meta_dict)
+
+    if meta_dict['clinvar']:
+        logger.info(f"[Step 3]: [{process_name}] :: ClinVar Baby!!")
+        meta_dict['variants'] = {}
+
+    logger.info(f"[Step 3]: [{process_name}] :: {meta_dict}")
+
+    return meta_dict
 
 # Step 3 - Perform statistics on Ditto
 def statistics(ditto_base_path, metadata_file, chromosome):
     """ """
+    logger.info(f"[Step 3]: Opening and loading {metadata_file} into a python dict")
     with open(metadata_file, 'r') as ditto_meta_file:
             meta_dict = json.load(ditto_meta_file)
 
+    logger.info(f"[Step 3]: Starting multiprocess Ditto variant fetcher")
     with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
         try:
             jobs = executor.map(worker, [(ditto_base_path, meta_dict[chromosome][gene]) for gene in meta_dict[chromosome].keys()])
 
             for job in jobs:
-                logger.info(f"MAIN :: Cleanup, deleting job... {job}")
+                logger.info(f"[Step 3]: Cleanup, deleting job... {job}")
 
                 chrom = job['chrom']
                 gene = job['gene']
@@ -318,7 +326,7 @@ def statistics(ditto_base_path, metadata_file, chromosome):
                 meta_dict[chrom][gene] = job
         
         except Exception as e:
-            logger.info(f"MAIN :: EXCEPTION :: {e}")
+            logger.info(f"[Step 3]: EXCEPTION :: {e}")
 
     with open(metadata_file, 'w') as ditto_meta_file:
         json.dump(meta_dict, ditto_meta_file, indent=4)
@@ -335,7 +343,7 @@ def main(args):
 
     ## Steps ##
     # 0: Setup
-    print("Step 0: Ditto filter setup")
+    print("[ MAIN ] :: Step 0 :: Ditto filter setup")
     ditto_full_path = f'{ditto_base_path}/raw/{chromosome}/'
     ditto_file_paths = [ditto_full_path + file for file in os.listdir(ditto_full_path)]
 
@@ -344,18 +352,18 @@ def main(args):
         with open(metadata_file, 'x') as file:
             file.write(json.dumps({}))
     except FileExistsError:
-        print(f"The file '{metadata_file}' already exists.")
+        logger.info(f"The file '{metadata_file}' already exists.")
 
     # 1: Gather metadata information on Ditto gene files
-    print("Step 1: Gathering Metadata")
+    logger.info("[ MAIN ] :: Step 1 :: Gathering Metadata")
     metadata(metadata_file, ditto_file_paths)
     
     # 2: Match ClinVar to Ditto and get statistics
-    print("Step 2: Fetching scores for ClinVar Path/Likely Path variants and running statistics")
-    clinvar(ditto_base_path, metadata_file, clinvar_base_path, chromosome)
+    logger.info("[ MAIN ] :: Step 2 :: Fetching scores for ClinVar Path/Likely Path variants")
+    clinvar_match(ditto_base_path, metadata_file, clinvar_base_path, chromosome)
     
-    # 3: Perform statistics
-    print("Step 3: Perform statistics on DITTO")
+    # # 3: Perform statistics
+    logger.info("[ MAIN ] :: Step 3 :: Perform statistics on DITTO")
     statistics(ditto_base_path, metadata_file, chromosome)
 
     return
@@ -372,4 +380,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     main(args)
-
